dc0019a60eb9f22b8d65ddfc32d354dc920bd532
Jonathan Searra <jonathan.searra@gmail.com>
2023-03-28 18:49:07 +0200

    Paging working one-way

--- GBTec/src/app/search/search.component.ts
+++ GBTec/src/app/search/search.component.ts
@@ -9,27 +9,37 @@ import { UnsplashService } from '../unsplash.service';
 })
 export class SearchComponent implements OnInit {
   query: string = '';
-  page: number = 1;
   photos: any[] = [];
+  page: number = 1;
+  photosToShow: any[] = [];
 
   constructor(private unsplashService: UnsplashService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
-  search() {
-    this.unsplashService.searchPhotos(this.query, this.page).subscribe((res: any) => {
-      this.photos = res.results;
-    });
+  searchPhotos(): void {
+    if (this.query.trim() === '') {
+      return;
+    }
+
+    this.unsplashService.searchPhotos(this.query, this.page)
+      .subscribe(
+        (data: any) => {
+          this.photos = data.results;
+          this.photosToShow = this.photos.slice(0, 10); // Display only the first 10 images by default
+        },
+        (error: any) => console.log(error)
+      );
   }
 
-  nextPage() {
+  loadMore(): void {
+    
+    console.log('Before', this.photos.length);
+    //this.photosToShow = this.photos.slice(0, 20);
     this.page++;
-    this.search();
-  }
-
-  prevPage() {
-    this.page--;
-    this.search();
+    this.searchPhotos()
+  console.log('After', this.photos.length); 
+  
   }
 }
